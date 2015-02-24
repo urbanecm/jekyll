f5618e27701cd387b57eb964f673ed68e5bdc78a
Martin Urbanec <martin@urbanec.cz>
2015-02-24 17:12:25 +0000

    Oprava slova v menu + snaha o přidání mapy ostatních projektů

---
+++ redirects/5.php
@@ -0,0 +1,13 @@
+<html>
+	{% include head.html %}
+        <body>
+		<h2>Ostatní projekty</h2>
+		<h3>Další projekty zajímající se o problematiku PAS</h3>
+		<ul>
+			<li><a href="http://mujautismus.cz">Mujautismus.cz</a></li>
+			<li><a href="http://zrzava-holka.blogspot.cz">zrzava-holka.blogspot.cz</a></li>
+			<li><a href="http://adventor.org">Adventor.org</a></li>
+			<li><a href="http://aspergerka-bloguje.blog.cz">Aspergerka-bloguje.blog.cz</a></li>
+			<h3>Jste autor nějakého méně známého projektu? Znáte nějaký prospěšný? V každém případě napište na <a href="mailto:info@asperger.cz">info@asperger.cz</a> a určitě se domluvíme!</h3>
+	</body>
+</html>
