f7204de46d28ca087a285c8b50ab37012c5bea82
Martin Urbanec <martin@urbanec.cz>
2015-03-15 07:51:08 +0000

    Změna názvů blogů na skutečný, nikoliv pouze adresu URL

--- redirects/5.php
+++ redirects/5.php
@@ -5,9 +5,10 @@
 		<h3>Další projekty zajímající se o problematiku PAS</h3>
 		<ul>
 			<li><a href="http://mujautismus.cz">Mujautismus.cz</a></li>
-			<li><a href="http://zrzava-holka.blogspot.cz">zrzava-holka.blogspot.cz</a></li>
+			<li><a href="http://zrzava-holka.blogspot.cz">Zrzavá holka</a></li>
 			<li><a href="http://adventor.org">Adventor.org</a></li>
-			<li><a href="http://aspergerka-bloguje.blog.cz">Aspergerka-bloguje.blog.cz</a></li>
+			<li><a href="http://aspergerka-bloguje.blog.cz">Pocity a trable holky s AS</a></li>
+			<li><a href="http://uvahyaspika.blogspot.cz">Úvahy aspíka</a></li>
 			<h3>Jste autor nějakého méně známého projektu? Znáte nějaký prospěšný? V každém případě napište na <a href="mailto:info@asperger.cz">info@asperger.cz</a> a určitě se domluvíme!</h3>
 	</body>
 </html>
