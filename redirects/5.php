28020c8fb894591a55b95ee8e00d87df9d581d32
Martin Urbanec <martin@urbanec.cz>
2015-03-31 19:09:15 +0000

    Oprava redirekce (na stránku v joomle)

--- redirects/5.php
+++ redirects/5.php
@@ -1,14 +1,3 @@
-<html>
-	{% include head.html %}
-        <body>
-		<h2>Ostatní projekty</h2>
-		<h3>Další projekty zajímající se o problematiku PAS</h3>
-		<ul>
-			<li><a href="http://mujautismus.cz">Mujautismus.cz</a></li>
-			<li><a href="http://zrzava-holka.blogspot.cz">Zrzavá holka</a></li>
-			<li><a href="http://adventor.org">Adventor.org</a></li>
-			<li><a href="http://aspergerka-bloguje.blog.cz">Pocity a trable holky s AS</a></li>
-			<li><a href="http://uvahyaspika.blogspot.cz">Úvahy aspíka</a></li>
-			<h3>Jste autor nějakého méně známého projektu? Znáte nějaký prospěšný? V každém případě napište na <a href="mailto:info@asperger.cz">info@asperger.cz</a> a určitě se domluvíme!</h3>
-	</body>
-</html>
+<?php
+header("Location: http://asperger.cz/joomla/index.php/12-pages/80-co-sledovat")
+?>
